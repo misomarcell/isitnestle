eae0e17d0c2a5814c2f17964f2f35569d62bc18e
Marcell Misó <misomarcell@gmail.com>
2022-01-24 22:57:22 +0100

    first prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+	AfterViewInit,
+	ChangeDetectorRef,
+	Component,
+	OnInit,
+	ViewChild,
+} from '@angular/core';
+import Quagga, { QuaggaJSConfigObject } from '@ericblade/quagga2';
 import { BarcodeScannerLivestreamComponent } from 'ngx-barcode-scanner';
 
 @Component({
@@ -9,13 +16,56 @@ import { BarcodeScannerLivestreamComponent } from 'ngx-barcode-scanner';
 export class AppComponent implements AfterViewInit {
 	@ViewChild(BarcodeScannerLivestreamComponent)
 	barcodeScanner: BarcodeScannerLivestreamComponent;
+	constructor(private changeDetector: ChangeDetectorRef) {}
 
 	title = 'isitlestle';
-	barcodeValue = undefined;
+	started = false;
+	barcodeValue: string;
+	devices: MediaDeviceInfo[];
+	selectedDeviceId: string;
+	type: ['ean_reader', 'ean_8_reader'];
+	config: Partial<QuaggaJSConfigObject> = {
+		decoder: {
+			readers: ['ean_reader', 'ean_8_reader', 'ean_5_reader'],
+		},
+		debug: true,
+	};
 
-	ngAfterViewInit(): void {
-		console.log('Starting');
+	async ngAfterViewInit(): Promise<void> {
+		this.barcodeScanner.config = this.config;
 		this.barcodeScanner.start();
+		this.started = true;
+	}
+
+	async changeCamera(deviceId: string): Promise<void> {
+		console.log(`Changing camera to ${deviceId}`);
+		this.config.inputStream = {
+			constraints: {
+				deviceId,
+				facingMode: 'environment',
+			},
+		};
+
+		await Quagga.CameraAccess.release();
+
+		//this.barcodeScanner.deviceId = deviceId;
+
+		// this.barcodeValue = 'Restarted';
+		//this.selectedDeviceId = deviceId;
+
+		// this.changeDetector.detectChanges();
+		if (this.started) {
+			await this.barcodeScanner.restart();
+		} else {
+			await this.barcodeScanner.start();
+			this.started = true;
+		}
+
+		// this.barcodeScanner.restart();
+	}
+
+	restart(): void {
+		this.barcodeScanner.restart();
 	}
 
 	onValueChanges(result: any): void {
@@ -23,7 +73,8 @@ export class AppComponent implements AfterViewInit {
 		console.log({ result });
 	}
 
-	onStarted(started: any): void {
-		console.log(started);
+	async onStarted(started: any): Promise<void> {
+		this.devices = await Quagga.CameraAccess.enumerateVideoDevices();
+		this.changeDetector.detectChanges();
 	}
 }
